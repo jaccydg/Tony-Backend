a82805a50bfe1a053bd6338829942ac31f8966b0
Giulio <callegarin.giulio02@gmail.com>
2024-06-07 23:09:34 +0200

    Pre-Migrations

--- Tony-Backend.API/Program.cs
+++ Tony-Backend.API/Program.cs
@@ -1,6 +1,18 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Tony_Backend.Auth.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// Add Auth Provider
+builder.Services.AddDbContext<AuthDbContext>(options => {
+    options.UseNpgsql(builder.Configuration.GetConnectionString("postgres"));
+});
+builder.Services.AddAuthorization();
+builder.Services.AddIdentityApiEndpoints<IdentityUser>()
+    .AddEntityFrameworkStores<AuthDbContext>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -8,6 +20,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.MapIdentityApi<IdentityUser>();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
---
+++ Tony-Backend.Auth/Data/AuthDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tony_Backend.Auth.Data
+{
+    public class AuthDbContext : IdentityDbContext<IdentityUser>
+    {
+        public AuthDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
