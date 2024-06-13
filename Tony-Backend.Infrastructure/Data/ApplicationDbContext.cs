88dea907858c7dcbba27bfeac8c7b9456b42ee62
Jacopo Di Gregorio <i.jaccy.i@gmail.com>
2024-06-13 13:49:07 +0200

    better looking

--- Tony-Backend.Infrastructure/Data/ApplicationDbContext.cs
+++ Tony-Backend.Infrastructure/Data/ApplicationDbContext.cs
@@ -30,77 +30,21 @@ namespace Tony_Backend.API.Data
 
 
             // SEED DATA
-
             modelBuilder.Entity<Gateway>().HasData(
-                new Gateway
-                {
-                    Id = 1,
-                    Name = "Gateway 1",
-                    Latitude = 48.8566,
-                    Longitude = 2.3522
-                },
-                new Gateway
-                {
-                    Id = 2,
-                    Name = "Gateway 2",
-                    Latitude = 48.8566,
-                    Longitude = 2.3512
-                },
-                new Gateway
-                {
-                    Id = 3,
-                    Name = "Gateway 3",
-                    Latitude = 48.8566,
-                    Longitude = 2.3589
-                }) ;
-
+                new Gateway { Id = 1, Name = "Gateway 1", Latitude = 48.8566, Longitude = 2.3522 },
+                new Gateway { Id = 2, Name = "Gateway 2", Latitude = 48.8566, Longitude = 2.3512 },
+                new Gateway { Id = 3, Name = "Gateway 3", Latitude = 48.8566, Longitude = 2.3589 });
 
             modelBuilder.Entity<ChargingStation>().HasData(
-                new ChargingStation
-                {
-                    Number = 1,
-                    GatewayId = 1
-                },
-                new ChargingStation
-                {
-                    Number = 2,
-                    GatewayId = 1
-                },
-                new ChargingStation
-                {
-                    Number = 1,
-                    GatewayId = 2
-                },
-                new ChargingStation
-                {
-                    Number = 2,
-                    GatewayId = 2
-                },
-                new ChargingStation
-                {
-                    Number = 3,
-                    GatewayId = 2
-                },
-                new ChargingStation
-                {
-                    Number = 1,
-                    GatewayId = 3
-                },
-                new ChargingStation
-                {
-                    Number = 2,
-                    GatewayId = 3
-                },
-                new ChargingStation
-                {
-                    Number = 3,
-                    GatewayId = 3
-                },
-                new ChargingStation
-                {
-                    Number = 4,
-                    GatewayId = 3
-                });
+                new ChargingStation { Number = 1, GatewayId = 1 },
+                new ChargingStation { Number = 2, GatewayId = 1 },
+                new ChargingStation { Number = 1, GatewayId = 2 },
+                new ChargingStation { Number = 2, GatewayId = 2 },
+                new ChargingStation { Number = 3, GatewayId = 2 },
+                new ChargingStation { Number = 1, GatewayId = 3 },
+                new ChargingStation { Number = 2, GatewayId = 3 },
+                new ChargingStation { Number = 3, GatewayId = 3 },
+                new ChargingStation { Number = 4, GatewayId = 3 });
         }
 
     }
