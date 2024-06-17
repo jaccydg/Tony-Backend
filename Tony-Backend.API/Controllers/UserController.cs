4f86d6050999929ffe59ed67eaea229406893525
Jacopo Di Gregorio <i.jaccy.i@gmail.com>
2024-06-17 16:03:56 +0200

    added user command

--- Tony-Backend.API/Controllers/UserController.cs
+++ Tony-Backend.API/Controllers/UserController.cs
@@ -9,21 +9,22 @@ using Tony_Backend.API.Data;
 using Tony_Backend.Shared.Entities;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 using System.Security.Claims;
+using Tony_Backend.Application.Commands.UserCommands;
 
 [ApiController]
 [Route("[controller]")]
 public class UserController : ControllerBase
 {
-    private readonly ApplicationDbContext _context;
+    private readonly ISender _sender;
 
-    public UserController(ApplicationDbContext context)
+    public UserController(ISender sender)
     {
-        _context = context;
+        _sender = sender;
     }
 
     [HttpGet(nameof(GetAllUsers))]
     public async Task<ActionResult<IEnumerable<IdentityUser>>> GetAllUsers()
     {
-        return await _context.Users.ToListAsync();
+        return Ok(await _sender.Send(new GetAllUsersCommand()));
     }
 }
---
+++ Tony-Backend.Application/Commands/UserCommands/GetAllUsersCommand.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tony_Backend.API.Data;
+using Tony_Backend.Shared.Entities;
+
+namespace Tony_Backend.Application.Commands.UserCommands
+{
+    public class GetAllUsersCommand : IRequest<IEnumerable<IdentityUser>> { }
+
+    internal class GetAllUsersCommandHandler : IRequestHandler<GetAllUsersCommand, IEnumerable<IdentityUser>>
+    {
+        private readonly ApplicationDbContext _context;
+        public GetAllUsersCommandHandler(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<IdentityUser>> Handle(GetAllUsersCommand request, CancellationToken cancellationToken)
+        {
+            return await _context.Users.ToListAsync(cancellationToken);
+        }
+    }
+}
