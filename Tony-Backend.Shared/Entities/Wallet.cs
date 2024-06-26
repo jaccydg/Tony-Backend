3e9450e2c61419066ed0c2fadfdcef6d62530750
Jonny <134015584+Jonninoo@users.noreply.github.com>
2024-06-26 13:03:37 +0200

    Added Entities ChargingSession, Gateway, Subscription, TopUpWalletcs, Wallet

---
+++ Tony-Backend.Shared/Entities/Wallet.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tony_Backend.Shared.Entities;
+
+namespace Tony_Backend.Shared.Entities
+{
+    public class Wallet
+    {
+        public Guid Id { get; set; }
+        public decimal SubscriptionCredit { get; set; } = 0;
+        public decimal PayPerUseCredit { get; set; } = 0;
+    }
+}
