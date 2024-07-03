cdb0e510d1b1f036d485f971fb789b9b34ad741b
Jacopo Di Gregorio <i.jaccy.i@gmail.com>
2024-07-03 17:10:09 +0200

    Update deploy file migraiton.sh

--- migration.sh
+++ migration.sh
@@ -2,7 +2,7 @@
 cd /home/ubuntu/tony/Tony-Backend.Infrastructure
 export PATH="$PATH:$HOME/.dotnet/tools/"
 # leave only last line once all migrations are done
-dotnet ef database drop
+dotnet ef database drop -f
 rm -r Migrations
 dotnet ef migrations add Initial
 dotnet ef database update
